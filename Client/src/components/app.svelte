4011dfccf040755da80a1b05d00fb8334a97f145
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2020-11-06 20:43:47 +0100

    Starting F7Svelte template

---
+++ Client/src/components/app.svelte
@@ -0,0 +1,40 @@
+<App params={ f7params } themeDark>
+
+  <!-- Your main view, should have "view-main" class -->
+  <View main class="safe-areas" url="/" />
+
+</App>
+<script>
+  import { onMount } from 'svelte';
+
+  import {
+    f7,
+    f7ready,
+    App,
+    View,
+  } from 'framework7-svelte';
+
+  import routes from '../js/routes';
+
+  // Framework7 Parameters
+  let f7params = {
+    name: 'IndySMS', // App name
+    theme: 'auto', // Automatic theme detection
+
+
+
+    // App routes
+    routes: routes,
+    // Register service worker
+    serviceWorker: {
+      path: '/service-worker.js',
+    },
+  };
+
+  onMount(() => {
+    f7ready(() => {
+
+      // Call F7 APIs here
+    });
+  })
+</script>
