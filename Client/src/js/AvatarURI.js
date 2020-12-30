8eee900a318b3ab4b3257381f755f7c86f01be96
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2020-12-30 11:40:40 +0100

    Function for data URI svg base64 avatar

---
+++ Client/src/js/AvatarURI.js
@@ -0,0 +1,26 @@
+/**
+ * Avatars node module documentation:
+ * https://www.npmjs.com/package/@dicebear/avatars-jdenticon-sprites
+*/
+
+import Avatars from '@dicebear/avatars';
+import sprites from '@dicebear/avatars-jdenticon-sprites';
+
+const options = {
+    hues: 256,
+    width: 300,
+    //height: 300,
+    colorLightness: [0.1,0.8],
+    grayscaleLightness: [0.2,0.7],
+    colorSaturation: 1,
+    grayscaleSaturation: 1,
+};
+
+const avatars = new Avatars(sprites, options);
+
+export function avatar (id) {
+    const svg = avatars.create(id);
+    const b64 = btoa(svg);
+    const uri = `data:image/svg+xml;base64,${b64}`;
+    return uri;
+}
