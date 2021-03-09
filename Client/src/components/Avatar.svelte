71377dc4e295357d1f74f55b6dde3eca6bd3b864
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2021-03-09 18:02:07 +0100

    Change avatar generator

--- Client/src/components/Avatar.svelte
+++ Client/src/components/Avatar.svelte
@@ -1,6 +1,7 @@
 <script>
     import Avatars from '@dicebear/avatars';
-    import sprites from '@dicebear/avatars-jdenticon-sprites';
+    //import sprites from '@dicebear/avatars-jdenticon-sprites';
+    import sprites from '@dicebear/avatars-identicon-sprites';
 
     export var id;
     export var size = null;
@@ -11,13 +12,14 @@
     */
 
     let options = {
-        hues: 256,
+        //hues: 256,
         width: size || 300,
         //height: 300,
-        colorLightness: [0.1,0.8],
-        grayscaleLightness: [0.2,0.7],
-        colorSaturation: 1,
-        grayscaleSaturation: 1,
+        //colorLightness: [0.1,0.8],
+        //grayscaleLightness: [0.2,0.7],
+        //colorSaturation: 1,
+        //grayscaleSaturation: 1,
+        colorLevel: 50,
     };
     let avatars = new Avatars(sprites, options);
     let svg = avatars.create(id);
