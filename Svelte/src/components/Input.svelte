ca89ee59e9057987dffccd42cfe7e92361815f53
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2021-03-11 13:08:02 +0100

    Home view draft

---
+++ Svelte/src/components/Input.svelte
@@ -0,0 +1,16 @@
+<script>
+    export var type = "text";
+    export var placeholder = "...";
+</script>
+
+<input type="{type}" placeholder="{placeholder}"/>
+
+<style>
+    input {
+        border-style: none;
+        border-radius: 0.5em;
+        background-color: pink;
+        padding: 1em;
+        font-weight: bold;
+    }
+</style>
