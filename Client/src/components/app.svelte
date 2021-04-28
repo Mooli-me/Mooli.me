fdb447be732af4b599c31b910b0f5eef5cbb572c
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2021-04-28 12:30:26 +0200

    Closes #107

--- Client/src/components/app.svelte
+++ Client/src/components/app.svelte
@@ -41,13 +41,13 @@
     },
     // For keep url in browsers
     view: {
-      pushState: true,
-      //pushStateRoot: window.location.hostname === 'localhost' ? 'http://localhost:8080' : 'https://mooli.me',
-      pushStateRoot: location.origin,
-      pushStateSeparator: '',
-      //browserHistory: true,
-      //browserHistoryRoot: window.location.hostname === 'localhost' ? 'http://localhost:8080' : 'https://mooli.me',
-      //browserHistorySeparator: '',
+      //pushState: true,
+      ////pushStateRoot: window.location.hostname === 'localhost' ? 'http://localhost:8080' : 'https://mooli.me',
+      //pushStateRoot: location.origin,
+      //pushStateSeparator: '',
+      browserHistory: true,
+      browserHistoryRoot: window.location.hostname === 'localhost' ? 'http://localhost:8080' : 'https://mooli.me',
+      browserHistorySeparator: '',
     }
   };
 
