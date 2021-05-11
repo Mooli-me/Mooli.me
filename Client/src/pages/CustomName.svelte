3f9908b8d7c39f0009ce62a17aa51f342b413568
Daniel Banore Dopico <daniel.banobre.dopico@protonmail.com>
2021-05-11 18:17:05 +0200

    Fix #123

--- Client/src/pages/CustomName.svelte
+++ Client/src/pages/CustomName.svelte
@@ -51,9 +51,9 @@
     </Navbar>
 
     <PageContent class="display-flex flex-direction-column justify-contents-center align-content-space-around align-contents-center" style="padding-top: 0px;">
-        <Avatar id={id} name={$names[id] || id}/>
+        <Avatar id={id} name={$names[id] || '...'}/>
         <form on:submit={submitHandler}>
-            <input type="text" placeholder="Escribe tu nombre para {id}..." bind:value={name}/>
+            <input type="text" placeholder="..." bind:value={name}/>
             <p>El nombre que tú elijas se almacenará sólo en tu teléfono. No se compartirá con nandie.</p>
             <input type="submit" value="Listo!">
         </form>
